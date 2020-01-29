b3735c1ecee26c396c918a5f060922aa788f2324
Adam Senn <asenn2@dxc.com>
2020-01-29 17:22:00 -0500

    Form submit working

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
-const App = () => (
-  <>
-    <h1>The IT list</h1>
-    <form>
-      <fieldset>
-        <legend>Get on the list!</legend>
-        <input type='text' name='name' placeholder='Name' autoFocus />
-        <button type='button'>Add Me Now</button>
-      </fieldset>
-    </form>
-    <ul>
-      <li>So lonely...</li>
-    </ul>
-  </>
-)
+const App = () => {
+  const [state, setState] = useState(['Johnny', 'Suzy'])
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    const form = event.target
+    setState([...state, form.name.value])
+    form.reset()
+  }
+
+  return (
+    <>
+      <h1>The IT list</h1>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <legend>Get on the list!</legend>
+          <input type='text' name='name' placeholder='Name' autoFocus />
+          <button type='button'>Add Me Now</button>
+        </fieldset>
+      </form>
+      <ul>
+        {state.map(item => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  )
+}
 
 export default App
