1d7d9e3cda912a1ff1ec519892b9271d9549f337
Adam Senn <asenn2@dxc.com>
2020-01-29 17:04:16 -0500

    More form setup

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import './App.css'
 
 const App = () => (
   <>
-    <h1>Get on the list!</h1>
+    <h1>The IT list</h1>
     <form>
-      <label>Name:</label>
-      <input type='text' name='name' placeholder='Name' />
-      <button type='button'>Add Me Now</button>
+      <fieldset>
+        <legend>Get on the list!</legend>
+        <input type='text' name='name' placeholder='Name' autoFocus />
+        <button type='button'>Add Me Now</button>
+      </fieldset>
     </form>
+    <ul>
+      <li>So lonely...</li>
+    </ul>
   </>
 )
 
