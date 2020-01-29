13238d8d4eac32f6cf9cad32efe3a6f7a804625d
Adam Senn <asenn2@dxc.com>
2020-01-29 18:45:42 -0500

    Added working grid

--- src/Posts.js
+++ src/Posts.js
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from 'react'
-import './App.css'
 import { getPostsAsync } from './ApiService'
+
+import BootstrapTable from 'react-bootstrap-table-next'
+import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit'
+import paginationFactory from 'react-bootstrap-table2-paginator'
+
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css'
+import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css'
 
 const Posts = () => {
   const [state, setState] = useState({ loading: true, posts: [] })
@@ -17,16 +23,35 @@ const Posts = () => {
 
   return (
     <>
-      <h1>Posts - {state.posts.length}</h1>
-      <ul>
-        {state.posts.map(item => (
-          <li key={item.id}>
-            {item.user.name} wrote '{item.title}'
-          </li>
-        ))}
-      </ul>
+      <ToolkitProvider keyField='id' columns={columns} data={state.posts} search>
+        {toolkitProps => (
+          <>
+            <header style={{ display: 'flex', justifyContent: 'space-between', marginTop: '1em' }}>
+              <h1>Posts - {state.posts.length}</h1>
+              <Search.SearchBar {...toolkitProps.searchProps} placeholder={'Keyword Search'} />
+            </header>
+            <BootstrapTable
+              {...toolkitProps.baseProps}
+              bootstrap4
+              bordered={false}
+              condensed
+              pagination={paginationFactory({
+                showTotal: true,
+                hideSizePerPage: false,
+                sizePerPage: 10
+              })}
+              striped
+            />
+          </>
+        )}
+      </ToolkitProvider>
     </>
   )
 }
+
+const columns = [
+  { dataField: 'user.name', text: 'Blogger', sort: true },
+  { dataField: 'title', text: 'Category', sort: true }
+]
 
 export default Posts
