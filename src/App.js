d23ea180c2355161e840e86cd57edba4683ef915
Adam Senn <asenn2@dxc.com>
2020-01-29 16:52:20 -0500

    Updated deps

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return
+  ;<>
+    <h1>Super Rad!</h1>
+    <form>
+      <label>Name:</label>
+      <input type='text' name='name' placeholder='Name' />
+      <button type='button'>Add Me</button>
+    </form>
+  </>
 }
 
-export default App;
+export default App
