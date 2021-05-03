e2779c4906e20427ed4f45e8911e02afa8f21dcc
adamsenn <asenn2@dxc.com>
2021-05-03 17:09:09 -0400

    Fixed base64 encoding

--- netlify/functions/update-progress.js
+++ netlify/functions/update-progress.js
@@ -3,10 +3,11 @@ const fetch = require('node-fetch')
 exports.handler = async function (event, context) {
     const { subdomain, callbackUrl, sessionToken, progressInfo } = JSON.parse(event.body)
     const progressApiEndpoint = new URL([callbackUrl, `progress?sessionToken=${sessionToken}`], `https://${subdomain}.csod.com`)
+    const base64Credentials = Buffer.from(`${process.env.CSOD_USER}:${process.env.CSOD_PASSWORD}`).toString('base64')
 
     const response = await fetch(progressApiEndpoint, {
         method: 'POST',
-        headers: { Authorization: `Basic ${btoa(`${process.env.CSOD_USER}:${process.env.CSOD_PASSWORD}`)}` },
+        headers: { Authorization: `Basic ${base64Credentials}` },
         body: progressInfo
     })
 
