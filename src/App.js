a199ec412cf064aa99149288cba17f3a71957b55
adamsenn <asenn2@dxc.com>
2021-05-05 14:04:40 -0400

    Added commit to footer

--- src/App.js
+++ src/App.js
@@ -8,32 +8,35 @@ import CourseLauncher from './CourseLauncher'
 import './App.css'
 
 const App = () => (
-  <CourseLauncher />
-  // <Router>
-  //   <nav>
-  //     <ul>
-  //       <li>
-  //         <Link to='/bloggers'>Bloggers</Link>
-  //       </li>
-  //       <li>
-  //         <Link to='/posts'>Posts</Link>
-  //       </li>
-  //       <li>
-  //         <Link to='/cool-kids'>Cool Kids</Link>
-  //       </li>
-  //       <li>
-  //         <Link to='/launch'>Course Launcher</Link>
-  //       </li>
-  //     </ul>
-  //   </nav>
-  //   <Switch>
-  //     <Route exact path='/bloggers' component={Bloggers} />
-  //     <Route exact path='/posts' component={Posts} />
-  //     <Route exact path='/cool-kids' component={ItList} />
-  //     <Route exact path='/launch' component={CourseLauncher} />
-  //     <Redirect to='/launch' />
-  //   </Switch>
-  // </Router>
+  <>
+    <CourseLauncher />
+    {/* <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/bloggers'>Bloggers</Link>
+          </li>
+          <li>
+            <Link to='/posts'>Posts</Link>
+          </li>
+          <li>
+            <Link to='/cool-kids'>Cool Kids</Link>
+          </li>
+          <li>
+            <Link to='/launch'>Course Launcher</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route exact path='/bloggers' component={Bloggers} />
+        <Route exact path='/posts' component={Posts} />
+        <Route exact path='/cool-kids' component={ItList} />
+        <Route exact path='/launch' component={CourseLauncher} />
+        <Redirect to='/launch' />
+      </Switch>
+    </Router> */}
+    <footer>{process.env.REACT_APP_COMMIT || 'local'}</footer>
+  </>
 )
 
 export default App
